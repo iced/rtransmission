9a55d43166eca04b65fc4fa396c24617a4fe0a5d
Andrew Kirilenko <andrew.kirilenko@gmail.com>
2011-06-03 13:03:06 +0300

    adding copyright to new files

--- lib/rtransmission/field.rb
+++ lib/rtransmission/field.rb
@@ -1,3 +1,7 @@
+# Copyright (c) 2011, Andrew Kirilenko <andrew.kirilenko@gmail.com>
+# All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be found in the COPYING file.
+
 module RTransmission
   class Field
     def self.unmap(value)
